13de36053da1a6e9ef70b7d78bcb12b53e593b2b
Juan Camilo <juankmilobernalz@gmail.com>
2025-11-23 22:01:04 -0500

    Creacion de la pagina 404.html. configuracion de paciente.html (sin conexion a db) y conexion con la base de datos de MySQL

--- app/app.py
+++ app/app.py
@@ -1,10 +1,34 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import pymysql
 
 app=Flask(__name__)
 
+#Configuracion de Conexion
+connection=pymysql.connect(
+    host='localhost',
+    user='root',
+    password='2207Chumilo,',
+    database='DB_MEDIGESTION',
+    port=3306,
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+#Index
 @app.route('/')
 def index():
     return render_template('index.html')
+
+#Paciente
+#@app.route('/Paciente/<int:id_Paciente>')
+#def Paciente(id_Paciente):
+    #return render_template('paciente.html')
+
+
+#Error 404
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
 
 if __name__=='__main__':
     app.run(debug=True,port=5001)
